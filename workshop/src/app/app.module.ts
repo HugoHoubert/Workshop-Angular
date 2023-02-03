79d4de5659d0e837441de78c57670df9f2a67bb2
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-02-03 17:34:34 +0100

    feature workshop

---
+++ workshop/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComposantComponent } from './composant/composant.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComposantComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
